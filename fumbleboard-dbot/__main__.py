0b2cc54e5cd71b18537832637dd20d4abea70499
Triskeldeian <triskeldeian@hotmail.com>
2020-11-24 11:40:28 +0100

    Simple bot from main

---
+++ fumbleboard-dbot/__main__.py
@@ -0,0 +1,43 @@
+import argparse
+import logging
+import sys
+from dotenv import load_dotenv
+import os
+
+from discord.ext import commands
+import discord
+
+#################################
+# parse logging level from cli
+parser = argparse.ArgumentParser()
+parser.add_argument("-log", "--log", nargs='+', help="Provide logging level. Example --log debug'")
+
+loglevel = parser.parse_args(None).log
+
+# set standard logging to INFO
+loglevel = loglevel[0] if loglevel else 'INFO'
+numeric_level = getattr(logging, loglevel.upper())
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+
+#################################
+# instantiate logger to stdout
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=numeric_level, format=FORMAT,stream=sys.stdout)
+logging.info('Setting LOGLEVEL={} ({})'.format(loglevel,numeric_level))
+
+#################################
+# load config
+load_dotenv()
+bot_token = os.getenv('DISCORD_BOT_TOKEN')
+
+
+#################################
+# instantiate the bot
+bot = commands.Bot(command_prefix=commands.when_mentioned)
+
+
+
+########################################
+# run the bot
+bot.run(bot_token)
