c3bbadea63a842429a3d1487426457eb0c45ffac
Stefano Caiazza <caiazza@uni-mainz.de>
2020-12-31 20:56:49 +0100

    Setup updated

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fumbleboard-dbot",
-    version="0.0.1",
-    author="triskeldeian, ypnoschris",
+    version="1.0.0",
+    author="triskeldeian, ypnoschris, kidpixo",
     author_email="triskeldeian@hotmail.com",
     description="A Discord bot to access the fumble board",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/caiazza/fumbleboard-dbot",
+    url="https://github.com/triskeldeian/fumbleboard-dbot",
     packages=setuptools.find_packages(),
 )
